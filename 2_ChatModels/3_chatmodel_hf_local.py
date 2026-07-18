e0e21ad9c39a7ab79e473afe861b8b9243f6228e
Mansi Gharat <mansigharat1909@gmail.com>
2026-07-18 20:38:44 +0530

    LLm model

--- 2_ChatModels/3_chatmodel_hf_local.py
+++ 2_ChatModels/3_chatmodel_hf_local.py
@@ -1,7 +1,14 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+os.environ['HF_HOME'] = 'M:/huggingface_cache'
 
 llm = HuggingFacePipeline.from_model_id(
-    
+    repo_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task = "text-generation",
+    pipeline_kwargs = dict(
+        tempurature = 0.5,
+        max_new_tokens = 100
+    )
 )
-
-model = ChatHuggingFace(llm)
+model = ChatHuggingFace(llm = llm)
