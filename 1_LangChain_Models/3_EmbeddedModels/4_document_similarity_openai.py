cf2a3c815f401fbdbf4c72ed25b9fbc610529b9d
Mansi Gharat <mansigharat1909@gmail.com>
2026-07-19 21:10:28 +0530

    document similarity

--- 1_LangChain_Models/3_EmbeddedModels/4_document_similarity_openai.py
+++ 1_LangChain_Models/3_EmbeddedModels/4_document_similarity_openai.py
@@ -20,4 +20,10 @@ query = "tell me about virat kohli"
 doc_embeddings = embedding.embed_documents(documents)
 query_embedding = embedding.embed_query(query)
 
-print(cosine_similarity([query_embedding],doc_embeddings))
+scores = cosine_similarity([query_embedding],doc_embeddings)[0]
+
+index , score = sorted(list(enumerate(scores)),key = lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is : " , score)
