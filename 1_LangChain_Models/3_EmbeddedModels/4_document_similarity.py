53dab417faee63d1b0879a5001ddb71275033e09
Mansi Gharat <mansigharat1909@gmail.com>
2026-07-19 21:02:22 +0530

    Langchain Model

---
+++ 1_LangChain_Models/3_EmbeddedModels/4_document_similarity.py
@@ -0,0 +1,23 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model = "text-embedding-3-large",dimensions = 300)
+
+documents = [
+    "Sachin Tendulkar is widely regarded as one of the greatest batsmen in cricket history, having scored 100 international centuries across his career.",
+    "MS Dhoni captained India to victory in the 2011 Cricket World Cup and is known for his calm decision-making under pressure.",
+    "Virat Kohli has been one of the most consistent run-scorers in modern cricket, known for his aggressive batting and fitness standards.",
+    "Rohit Sharma holds the record for the highest individual score in ODI cricket, with 264 runs in a single innings.",
+    "Jasprit Bumrah is known for his unorthodox bowling action and has become one of the most feared fast bowlers in world cricket."
+]
+
+query = "tell me about virat kohli"
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+print(cosine_similarity([query_embedding],doc_embeddings))
