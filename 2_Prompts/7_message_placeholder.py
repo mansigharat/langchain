e00efbac93ec4556bec6104ee9e5b19f1948d9f7
Mansi Gharat <mansigharat1909@gmail.com>
2026-07-25 16:47:15 +0530

    message placeholder

--- 2_Prompts/7_message_placeholder.py
+++ 2_Prompts/7_message_placeholder.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
-from langchain_core.messages import HumanMessage
+
 #chat template
 chat_template = ChatPromptTemplate([
     ('system',"You are a helpful customer support agent"),
