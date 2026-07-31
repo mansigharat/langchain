fc3aeb6efcd4de8303b9cd4743de98dda2701311
Mansi Gharat <mansigharat1909@gmail.com>
2026-08-01 00:27:05 +0530

    runnable

--- 6_Runnables/simple_LLM_app.py
+++ 6_Runnables/simple_LLM_app.py
@@ -1,23 +1,17 @@
+from langchain.llms import Groq
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
-from langchain_core.prompts import PromptTemplate
-
 load_dotenv()
-
-llm = ChatGroq(
-    model="openai/gpt-oss-120b",
-    temperature=0.7
-)
+llm = Groq(model="openai/gpt-oss-120b",temperature = 0.7)
 
 prompt = PromptTemplate(
-    input_variables=["topic"],
-    template="Suggest a catchy blog title about {topic}"
+    input_variables = ['topic'],
+    template = "Suggest a catchy blog title about {topic}"
 )
+topic = input('Enter a Topic : ')
 
-topic = input("Enter a Topic: ")
+formatted_prompt = prompt.format(topic=topic)
 
-chain = prompt | llm
+blog_title = llm.predict(formatted_prompt)
 
-response = chain.invoke({"topic": topic})
-
-print("Generated Blog Title:", response.content)
+print("Generated Blog Title : ",blog_title)
