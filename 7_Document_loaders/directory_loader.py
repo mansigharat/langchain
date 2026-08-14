c08ab6f5d16658ce0a54f79d5783de6af8bf00ea
Mansi Gharat <mansigharat1909@gmail.com>
2026-08-14 23:39:18 +0530

    directory loaders

--- 7_Document_loaders/directory_loader.py
+++ 7_Document_loaders/directory_loader.py
@@ -1,1 +1,17 @@
-from langchain_community.document_laoders import DirectoryLoader,PyPDFLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+
+loader = DirectoryLoader(
+    path=r"7_Document_loaders\books",
+    glob="*.pdf",
+    loader_cls=PyPDFLoader
+)
+
+# docs = loader.load()
+docs = loader.lazy_load()
+
+# print(len(docs))
+# print(docs[0].page_content)
+# print(docs[0].metadata)
+
+for document in docs:
+    print(document.metadata)
